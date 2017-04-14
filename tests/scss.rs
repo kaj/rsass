c354b52d2e7d43913147412438b673368b92945d
Rasmus Kaj <kaj@kth.se>
2017-04-14 19:44:18 +0200

    Another test.

--- tests/scss.rs
+++ tests/scss.rs
@@ -2,6 +2,22 @@
 extern crate rsass;
 use rsass::{OutputStyle, compile_scss};
 
+#[test]
+fn alpha() {
+    check(b"$x: rgb(0, 255, 255);\n\n\
+            div {\n  color: rgb(255, $blue: 0, $green: 255);\n  \
+            background: rgb(123, 45, 6);\n//  \
+            flah: rgba(0, 0, 0, 1) + #111;\n  \
+            grah: rgba(#f0e, $alpha: .5);\n//  blah: rgba(1,2,3,.6);\n\n  \
+            floo: $x;\n//  bloo: rgba($x, 0.7);\n  groo: $x;\n\n  \
+            $x: rgb(123, 45, 6);\n\n  \
+            hoo: red($x);\n\n  moo: green($x);\n  poo: blue($x);\n\n\
+            //  goo: mix(rgba(255, 0, 0, 0.5), #00f);\n\n\
+            boo: invert(#123456);\n}\n",
+          "div {\n  color: yellow;\n  background: #7b2d06;\n  \
+           grah: rgba(255, 0, 238, 0.5);\n  floo: cyan;\n  groo: cyan;\n  \
+           hoo: 123;\n  moo: 45;\n  poo: 6;\n  boo: #edcba9;\n}\n")
+}
 
 #[test]
 fn important() {
