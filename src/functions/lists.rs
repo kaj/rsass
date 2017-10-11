f474ad32443bf699326ba12c140e74f81e6e1eed
Rasmus Kaj <kaj@kth.se>
2017-10-11 22:16:48 +0200

    Implent join list function.

--- src/functions/lists.rs
+++ src/functions/lists.rs
@@ -25,6 +25,38 @@ pub fn register(f: &mut BTreeMap<&'static str, SassFunction>) {
         list[i] = s.get("value");
         Ok(Value::List(list, sep, bra))
     });
+    def!(f, join(list1, list2, separator = b"auto", bracketed = b"auto"), |s| {
+        let (mut list1, sep1, bra1) = match s.get("list1") {
+            Value::List(list, sep, bra) => (list, Some(sep), bra),
+            v => (vec![v], None, false),
+        };
+        let (mut list2, sep2, _bra2) = match s.get("list2") {
+            Value::List(list, sep, bra) => (list, Some(sep), bra),
+            v => (vec![v], None, false),
+        };
+        let separator = match s.get("separator") {
+            Value::Literal(sep, _) => {
+                let sep = sep.to_lowercase();
+                if sep == "comma" {
+                    ListSeparator::Comma
+                } else if sep == "space" {
+                    ListSeparator::Space
+                } else if sep == "auto" {
+                    sep1.or(sep2).unwrap_or(ListSeparator::Space)
+                } else {
+                    return Err(Error::badarg("'comma', 'space', or 'auto'",
+                                             &Value::Literal(sep, Quotes::Double)));
+                }
+            }
+            other => return Err(Error::badarg("'comma', 'space', or 'auto'", &other)),
+        };
+        list1.append(&mut list2);
+        let bra = match s.get("bracketed") {
+            Value::Literal(ref s, _) if s == "auto" => bra1,
+            b => b.is_true(),
+        };
+        Ok(Value::List(list1, separator, bra))
+    });
     def!(f, append(list, val, separator), |s| {
         let (mut list, sep, bra) = match s.get("list") {
             Value::List(v, s, bra) => (v, Some(s), bra),
@@ -112,5 +144,64 @@ mod test {
     fn append_e() {
         assert_eq!(do_evaluate(&[], b"append((blue, red), green, space);"),
                    "blue red green")
+    }
+
+    mod join {
+        //! From `sass-spec/spec/core_functions/join/valid`
+
+        #[test]
+        fn both_bracketed() {
+            check_val("join([foo bar], [baz bang]);",
+                      "[foo bar baz bang]")
+        }
+        #[test]
+        fn first_bracketed() {
+            check_val("join([foo bar], baz bang);",
+                      "[foo bar baz bang]")
+        }
+        #[test]
+        fn second_bracketed() {
+            check_val("join(foo bar, [baz bang]);",
+                      "foo bar baz bang")
+        }
+        #[test]
+        fn bracketed_true() {
+            check_val("join(foo, bar, $bracketed: true);",
+                      "[foo bar]")
+        }
+        #[test]
+        fn bracketed_false() {
+            check_val("join([foo], [bar], $bracketed: false);",
+                      "foo bar")
+        }
+        #[test]
+        fn separator_and_bracketed() {
+            check_val("join(foo, bar, $separator: comma, $bracketed: true);",
+                      "[foo, bar]")
+        }
+        #[test]
+        fn bracketed_and_separator() {
+            check_val("join(foo, bar, $bracketed: true, $separator: comma);",
+                      "[foo, bar]")
+        }
+        #[test]
+        fn separator_and_bracketed_positional() {
+            check_val("join(foo, bar, comma, true);",
+                      "[foo, bar]")
+        }
+        #[test]
+        fn unusual_bracketed_type() {
+            check_val("join(foo, bar, $bracketed: foo);",
+                      "[foo bar]")
+        }
+        #[test]
+        fn bracketed_null() {
+            check_val("join([foo], [bar], $bracketed: null);",
+                      "foo bar")
+        }
+        fn check_val(src: &str, correct: &str) {
+            use variablescope::test::do_evaluate;
+            assert_eq!(do_evaluate(&[], src.as_bytes()), correct)
+        }
     }
 }
