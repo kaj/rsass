ac51daada65cb8b8f31b5d53c67edc9d5fef80c0
Rasmus Kaj <kaj@kth.se>
2018-04-29 15:27:21 +0200

    Test file structure.

---
+++ tests/css/main.rs
@@ -0,0 +1,31 @@
+//! Tests from `sass_spec/spec/css/`
+
+extern crate rsass;
+use rsass::{compile_scss, OutputStyle};
+
+mod unknown_directive;
+
+#[test]
+fn multi_star_comments() {
+    // Note, actual expected has single newlines, but the sass-spec
+    // test runner succeeds my implementation.
+    check(
+        "a /***/ b {x: y}\n\
+         a /****/ b {x: y}\n\
+         a /* **/ b {x: y}\n\
+         a /** */ b {x: y}\n",
+        "a b {\n  x: y;\n}\n\n\
+         a b {\n  x: y;\n}\n\n\
+         a b {\n  x: y;\n}\n\n\
+         a b {\n  x: y;\n}\n",
+    )
+}
+
+fn check(input: &str, expected: &str) {
+    assert_eq!(
+        compile_scss(input.as_bytes(), OutputStyle::Expanded)
+            .and_then(|s| Ok(String::from_utf8(s)?))
+            .unwrap(),
+        expected
+    );
+}
