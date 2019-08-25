818cbfcaac72d6e9119e034af392aa791962a5a4
Rasmus Kaj <kaj@kth.se>
2019-08-25 22:21:26 +0200

    Improve error handling in selector fns.
    Avoid panic in some error conditions.

--- src/functions/selector.rs
+++ src/functions/selector.rs
@@ -1,5 +1,6 @@
 use super::SassFunction;
 use crate::css::Value;
+use crate::error::Error;
 use crate::parser::selectors::{selector, selectors};
 use crate::selectors::{Selector, Selectors};
 use crate::value::Quotes;
@@ -12,12 +13,13 @@ pub fn register(f: &mut BTreeMap<&'static str, SassFunction>) {
                 "{}",
                 v.into_iter()
                     .map(parse_selectors)
-                    .fold(Selectors::root(), |b, e| e.inside(&b)),
+                    .try_fold(Selectors::root(), |b, e| e
+                        .map(|e| e.inside(&b)))?
             ),
             Quotes::None,
         )),
         v => Ok(Value::Literal(
-            format!("{}", parse_selectors(v)),
+            format!("{}", parse_selectors(v)?),
             Quotes::None,
         )),
     });
@@ -28,47 +30,59 @@ pub fn register(f: &mut BTreeMap<&'static str, SassFunction>) {
             Value::List(v, _, _) => Ok(Value::Literal(
                 format!(
                     "{}",
-                    v.into_iter().map(parse_selectors).fold(
+                    v.into_iter().map(parse_selectors).try_fold(
                         Selectors::root(),
-                        |base, ext| Selectors::new(
+                        |base, ext| ext.and_then(|ext| Ok(Selectors::new(
                             base.s
                                 .into_iter()
-                                .flat_map(|b| ext.s.iter().map(move |e| {
-                                    parse_selector(&format!("{}{}", b, e))
-                                }))
-                                .collect()
-                        ),
-                    ),
+                                .flat_map(|b| {
+                                    ext.s.iter().map(move |e| {
+                                        parse_selector(&format!("{}{}", b, e))
+                                    })
+                                })
+                                .collect::<Result<_, _>>()?
+                        ))),
+                    )?,
                 ),
                 Quotes::None,
             )),
             v => Ok(Value::Literal(
-                format!("{}", parse_selectors(v)),
+                format!("{}", parse_selectors(v)?),
                 Quotes::None,
             )),
         }
     );
     def!(f, selector_parse(selector), |s| Ok(parse_selectors(
         s.get("selector")?
-    )
+    )?
     .to_value()));
 }
 
-// Note, this helper should probably handle errors and return a Result.
-fn parse_selectors(v: Value) -> Selectors {
+fn parse_selectors(v: Value) -> Result<Selectors, Error> {
     let s = format!("{}", v.unquote());
     if s.is_empty() {
-        Selectors::root()
+        Ok(Selectors::root())
     } else {
-        let (rest, result) = selectors(s.as_bytes()).unwrap();
-        assert!(rest.is_empty() || rest == b",");
-        result
+        let (rest, result) = selectors(s.as_bytes())?;
+        if !rest.is_empty() && rest != b"," {
+            Err(Error::S(format!(
+                "Unexpected remains in parse_selectors: {:?}",
+                rest
+            )))
+        } else {
+            Ok(result)
+        }
     }
 }
 
-// Note, this helper should probably handle errors and return a Result.
-fn parse_selector(s: &str) -> Selector {
-    let (rest, result) = selector(s.as_bytes()).unwrap();
-    assert!(rest.is_empty());
-    result
+fn parse_selector(s: &str) -> Result<Selector, Error> {
+    let (rest, result) = selector(s.as_bytes())?;
+    if !rest.is_empty() {
+        Err(Error::S(format!(
+            "Unexpected remains in parse_selector: {:?}",
+            rest
+        )))
+    } else {
+        Ok(result)
+    }
 }
