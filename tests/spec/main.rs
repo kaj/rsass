a993885bf093c1c6780a5e161f7ec5d69eef020f
Rasmus Kaj <kaj@kth.se>
2020-09-19 20:44:57 +0200

    Update spec to 2020-09-17.

--- tests/spec/main.rs
+++ tests/spec/main.rs
@@ -1,9 +1,11 @@
 //! Tests auto-converted from "sass-spec/spec"
-//! version 73222792c, 2020-08-10 13:55:12 -0700.
+//! version 45b9cbaf0, 2020-09-17 17:50:20 -0700.
 //! See <https://github.com/sass/sass-spec> for source material.\n
 //! The following tests are excluded from conversion:
 //! ["core_functions/selector/extend", "core_functions/selector/is_superselector", "core_functions/selector/unify", "directives/forward", "directives/use", "libsass-closed-issues/issue_185/mixin.hrx", "libsass-todo-issues/issue_221262.hrx", "libsass-todo-issues/issue_221292.hrx", "libsass/Sa\u{301}ss-UT\u{327}F8.hrx", "libsass/unicode-bom/utf-16-big", "libsass/unicode-bom/utf-16-little", "non_conformant/scss/huge.hrx", "non_conformant/scss/mixin-content.hrx", "non_conformant/scss/multiline_var.hrx"]
 use rsass::{compile_scss, output::Format};
+
+mod arguments;
 
 mod core_functions;
 
