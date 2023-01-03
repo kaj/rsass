542ca4dfdcd93a41c6820a78d2f891defee116c1
Frederic Nieto <frederic.artus.nieto@gmail.com>
2023-01-03 23:16:39 +0100

    improved macro error

--- rsass-macro/src/lib.rs
+++ rsass-macro/src/lib.rs
@@ -2,15 +2,28 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::ToTokens;
-use syn::{LitStr, parse_macro_input};
+use syn::{Error, LitStr, parse_macro_input};
 use rsass::output::{Format, Style};
 
 #[proc_macro]
 pub fn scss(tokens: TokenStream) -> TokenStream {
     let input = parse_macro_input!(tokens as LitStr);
-
-    LitStr::new(core::str::from_utf8( &rsass::compile_scss( input.value().as_bytes(), Format {
+    let format = Format {
         style: Style::Compressed,
         precision: 10,
-    }).unwrap()).unwrap(), Span::call_site()).to_token_stream().into()
+    };
+    match rsass::compile_scss(input.value().as_bytes(), format) {
+        Ok(output) => {
+            let output = core::str::from_utf8(&output).unwrap();
+            LitStr::new(output, Span::call_site())
+                .to_token_stream()
+                .into()
+        }
+        Err(err) => {
+            let msg = format!("{err:?}");
+            Error::new(Span::call_site(), msg)
+                .into_compile_error()
+                .into()
+        }
+    }
 }
