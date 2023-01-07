58b9651503a6213b6be96af59fabfa64ce39683c
Rasmus Kaj <kaj@kth.se>
2023-01-07 20:26:05 +0100

    Add `include_scss!(path)` to rsass-macros.

--- rsass-macros/src/lib.rs
+++ rsass-macros/src/lib.rs
@@ -8,6 +8,8 @@ use syn::{parse_macro_input, Error, LitStr};
 
 /// Convert a scss string to css at compile time.
 ///
+/// The only argument is literal string of scss code.
+///
 /// # Examples
 ///
 /// ````
@@ -30,8 +32,42 @@ pub fn scss(tokens: TokenStream) -> TokenStream {
                 .into()
         }
         Err(err) => {
-            let msg = format!("{err:?}");
-            Error::new(Span::call_site(), msg)
+            Error::new(Span::call_site(), format!("{err:?}"))
+                .into_compile_error()
+                .into()
+        }
+    }
+}
+
+/// Load and convert a scss string to css at compile time.
+///
+/// The only argument is a path given as a literal string.
+/// The path is evaluated from the crate root of the code calling the
+/// macro (the directory containing your `Cargo.toml` file).
+///
+/// # Examples
+///
+/// ````
+/// use rsass_macros::include_scss;
+/// const CSS: &str = include_scss!("tests/example.scss");
+/// assert_eq!(CSS, "body{font-family:serif}\n");
+/// ````
+#[proc_macro]
+pub fn include_scss(tokens: TokenStream) -> TokenStream {
+    let path = parse_macro_input!(tokens as LitStr);
+    let format = Format {
+        style: Style::Compressed,
+        precision: 10,
+    };
+    match rsass::compile_scss_path(path.value().as_ref(), format) {
+        Ok(output) => {
+            let output = core::str::from_utf8(&output).unwrap();
+            LitStr::new(output, Span::call_site())
+                .to_token_stream()
+                .into()
+        }
+        Err(err) => {
+            Error::new(Span::call_site(), format!("{err:?}"))
                 .into_compile_error()
                 .into()
         }
