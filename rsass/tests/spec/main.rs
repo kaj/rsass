968f91a89a009f6b76588b66b2d0904dfda849eb
Rasmus Kaj <kaj@kth.se>
2023-08-21 23:23:19 +0200

    Update sass-spec to 2023-08-21.

--- rsass/tests/spec/main.rs
+++ rsass/tests/spec/main.rs
@@ -1,5 +1,5 @@
 //! Tests auto-converted from "sass-spec/spec"
-//! version 790ec468, 2023-08-17 12:02:50 -0700.
+//! version 74e22a01f, 2023-08-21 12:59:04 -0700.
 //! See <https://github.com/sass/sass-spec> for source material.\n
 //! The following tests are excluded from conversion:
 //! ["core_functions/selector/extend", "core_functions/selector/is_superselector", "core_functions/selector/unify", "directives/extend", "libsass-todo-issues/issue_221260.hrx", "libsass-todo-issues/issue_221262.hrx", "libsass-todo-issues/issue_221264.hrx", "libsass-todo-issues/issue_221292.hrx", "libsass/unicode-bom/utf-16-big", "libsass/unicode-bom/utf-16-little", "non_conformant/scss/huge.hrx", "non_conformant/scss/multiline-var.hrx"]
